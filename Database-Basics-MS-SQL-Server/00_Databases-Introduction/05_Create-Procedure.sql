0ccaf16cd42724474d694d1b5c56a7f3e6100e22
pavelNM <papi1998@mail.bg>
2018-05-07 13:05:13 +0300

    Added lab

---
+++ Database-Basics-MS-SQL-Server/00_Databases-Introduction/05_Create-Procedure.sql
@@ -0,0 +1,29 @@
+CREATE PROC p_AddAccount @ClientId INT, @AccountTypeId INT AS
+INSERT INTO Accounts (ClientId, AccountTypeId) VALUES (@ClientId, @AccountTypeId)
+
+p_AddAccount 2, 2
+
+CREATE PROC p_Deposit @AccountId INT, @Amount DECIMAL(15, 2) AS
+UPDATE Accounts
+SET Balance += @Amount
+WHERE Id = @AccountId
+
+p_Deposit 2, 33.33
+
+CREATE PROC p_Withdraw @AccountId INT, @Amount DECIMAL(15, 2) AS
+BEGIN
+	DECLARE @OldBalance DECIMAL(15, 2)
+	SELECT @OldBalance = Balance FROM Accounts WHERE Id = @AccountId
+
+	IF (@OldBalance - @Amount >= 0)
+	BEGIN
+		UPDATE Accounts
+		SET Balance -= @Amount
+		WHERE Id = @AccountId
+	END
+
+	ELSE
+	BEGIN
+		RAISERROR('Insufficient funds', 10, 1)
+	END
+END
